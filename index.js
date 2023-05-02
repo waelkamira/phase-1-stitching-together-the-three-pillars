d1c5b8876cfa96133069d3ad4a1def38184ea031
wael kamira <ramondm4@gmail.com>
2023-05-02 13:00:19 +0300

    Assignment Completed

--- index.js
+++ index.js
@@ -1,2 +1,12 @@
 /* This is a file inserted to keep the learn IDE browser happy */
 const testVar = {}
+function removeDuplicates(arr) {
+    const uniqueArr = [];
+    for (let i = 0; i < arr.length; i++) {
+      if (!uniqueArr.includes(arr[i])) {
+        uniqueArr.push(arr[i]);
+      }
+    }
+    return uniqueArr;
+  }
+  
