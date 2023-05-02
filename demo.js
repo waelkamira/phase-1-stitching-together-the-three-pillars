d4bea95c20d7581799ac1e34d743a63c8983cb66
wael kamira <ramondm4@gmail.com>
2023-05-02 13:56:23 +0300

    Assignment Completed

--- demo.js
+++ demo.js
@@ -1,3 +1,5 @@
+// We use the Objects below to control toggling like / unlike status
+
 const glyphStates = {
   "♡": "♥",
   "♥": "♡"
@@ -8,7 +10,11 @@ const colorStates = {
   "": "red"
 };
 
-const articleHearts = document.querySelectorAll(".like-glyph");
+// STEP 1: The line of code below is what lets JavaScript find the elements that
+// we want to make clickable. Without JavaScript, clicking on these heart shapes
+// does nothing. Uncomment the code and refresh the demo page. 
+
+// const articleHearts = document.querySelectorAll(".like-glyph");
 
 function likeCallback(e) {
   const heart = e.target;
@@ -20,17 +26,32 @@ function likeCallback(e) {
       // the sending of information to the server succeeds.
       alert("You notified the server!");
       // alert(serverMessage);
-      heart.innerText = glyphStates[heart.innerText];
-      heart.style.color = colorStates[heart.style.color];
+      // heart.innerText = glyphStates[heart.innerText];
+      // heart.style.color = colorStates[heart.style.color];
     })
     .catch(function(error) {
       alert("Something went wrong!");
     });
 }
 
-for (const glyph of articleHearts) {
-  glyph.addEventListener("click", likeCallback);
-}
+// STEP 3: In order for the call to the server and the update of the screen to
+// work, we need to add a click event listener to the elements we identified in
+// STEP 1. That's Pillar 2, event handling. Uncomment this code:
+
+// for (const glyph of articleHearts) {
+//   glyph.addEventListener("click", likeCallback);
+// }
+
+// STEP 4: 
+
+// When all the STEPs' code changes have been complete, you'll be able to see a
+// working demonstration of our reference example. Sure, it's maybe not as
+// pretty as a professional site, but they're only different from our site in
+// style, not substance.
+
+//------------------------------------------------------------------------------
+// Ignore after this point. Used only for demo purposes
+//------------------------------------------------------------------------------
 
 function mimicServerCall() {
   return new Promise(function(resolve, reject) {
--- index.js
+++ index.js
@@ -9,4 +9,45 @@ function removeDuplicates(arr) {
     }
     return uniqueArr;
   }
+  const glyphStates = {
+    "♡": "♥",
+    "♥": "♡"
+  };
+  
+  const colorStates = {
+    "red" : "",
+    "": "red"
+  };
+  
+  const articleHearts = document.querySelectorAll(".like-glyph");
+  
+  function likeCallback(e) {
+    const heart = e.target;
+    mimicServerCall()
+      .then(function(serverMessage){
+        // STEP 2: Uncomment the 3 lines after the alert.
+        // Here we're using Pillar 1 (DOM Manipulation) to update the screen and
+        // mimicking Pillar 3 (Server Communication) to only update the screen if
+        // the sending of information to the server succeeds.
+        alert("You notified the server!");
+        // alert(serverMessage);
+        heart.innerText = glyphStates[heart.innerText];
+        heart.style.color = colorStates[heart.style.color];
+      })
+      .catch(function(error) {
+        alert("Something went wrong!");
+      });
+  }
+  
+  for (const glyph of articleHearts) {
+    glyph.addEventListener("click", likeCallback);
+  }
+  
+  function mimicServerCall() {
+    return new Promise(function(resolve, reject) {
+      setTimeout(function() {
+        resolve("Pretend remote server notified of action!");
+      }, 300);
+    });
+  }
   
